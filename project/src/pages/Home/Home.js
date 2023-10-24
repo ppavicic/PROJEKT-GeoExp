18a3a7cf1ff1efb073dba104e013f5a6517272ec
pp53564 <petra.petrovic@fer.hr>
2023-10-24 12:03:45 +0200

    dodana mapa

--- project/src/pages/Home/Home.js
+++ project/src/pages/Home/Home.js
@@ -1,7 +1,44 @@
+import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
+import './Home.css';
+import 'leaflet/dist/leaflet.css';
+import L from "leaflet";
+import React, { useState, useEffect, useRef } from "react";
+
+
 
 
 export const Home = () => {
+
+   const LeafIcon = L.Icon.extend({
+      options: {}
+   });
+   const greenIcon = new LeafIcon({
+      iconUrl:
+         "https://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2%7C2ecc71&chf=a,s,ee00FFFF"
+   });
+
+   const blueIcon = new LeafIcon({
+      iconUrl:
+         "https://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2%7Cabcdef&chf=a,s,ee00FFFF"
+   });
+   const [iconB, setIconB] = useState(blueIcon);
+   const [iconG, setIconG] = useState(greenIcon);
    return (
-      <div>hej</div>
+      <body>
+         <div className="map-container">
+            <MapContainer center={[45.8, 15.97]} zoom={9} scrollWheelZoom={true} >
+               <TileLayer
+                  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+               />
+               <Marker position={[45.8, 15.97]} icon={iconB}>
+                  <Popup>
+                     A pretty CSS3 popup. <br /> Easily customizable.
+                  </Popup>
+               </Marker>
+            </MapContainer>
+         </div>
+      </body>
+
    )
 }
