3e5b7b892e8a3c82f83de7ebfa73b3c45ea6b104
ppavicic <petar.pavicic@fer.hr>
2023-10-28 10:39:44 +0200

    added styles folder for css; deleted home folder and unnecessary files; formatted some code

--- project/src/App.js
+++ project/src/App.js
@@ -1,7 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import { Home } from "./pages/Home/Home"
+import { Home } from "./pages/Home"
 
 function App() {
   return (
--- project/src/pages/Home.js
+++ project/src/pages/Home.js
@@ -1,37 +1,42 @@
 import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
-import './Home.css';
+import '../styles/Home.css';
 import 'leaflet/dist/leaflet.css';
 import L from "leaflet";
 import React, { useState, useEffect, useRef } from "react";
-
-
-
 
 export const Home = () => {
 
    const LeafIcon = L.Icon.extend({
       options: {}
    });
-   const greenIcon = new LeafIcon({
-      iconUrl:
-         "https://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2%7C2ecc71&chf=a,s,ee00FFFF"
-   });
+
+   const initialCenter = [45.8, 15.97];
+   const centerLatLng = L.latLng(initialCenter);
 
    const blueIcon = new LeafIcon({
       iconUrl:
-         "https://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2%7Cabcdef&chf=a,s,ee00FFFF"
+         "https://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2%7Cabcdef&chf=a,s,ee00FFFF",
+      iconSize: [25, 30],
    });
-   const [iconB, setIconB] = useState(blueIcon);
-   const [iconG, setIconG] = useState(greenIcon);
+
+   const greenIcon = new LeafIcon({
+      iconUrl:
+         "https://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2%7C2ecc71&chf=a,s,ee00FFFF",
+      iconSize: [25, 30]
+   });
+
+   const [iconA, setIconA] = useState(blueIcon);
+   const [iconB, setIconB] = useState(greenIcon);
+
    return (
       <body>
          <div className="map-container">
-            <MapContainer center={[45.8, 15.97]} zoom={9} scrollWheelZoom={true} >
+            <MapContainer center={initialCenter} zoom={9} scrollWheelZoom={true} minZoom={4}>
                <TileLayer
                   attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                   url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                />
-               <Marker position={[45.8, 15.97]} icon={iconB}>
+               <Marker position={initialCenter} icon={iconA} >
                   <Popup>
                      A pretty CSS3 popup. <br /> Easily customizable.
                   </Popup>
@@ -39,6 +44,5 @@ export const Home = () => {
             </MapContainer>
          </div>
       </body>
-
    )
 }
