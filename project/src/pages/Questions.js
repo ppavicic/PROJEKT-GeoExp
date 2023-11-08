39f33db201bf2dee19fbc463eba072e8163b9971
Dominik Zoricic <dominik.zoricic10000@gmail.com>
2023-11-08 20:16:05 +0100

    Added answers

--- project/src/pages/Questions.js
+++ project/src/pages/Questions.js
@@ -4,42 +4,60 @@ import "../styles/Question.css";
 import { useNavigate } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor((Math.random() * (i + 1)) % 4);
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
 function Question() {
-  const [value, setAnswer] = useState('');
-  const [question, setQuestion] = useState('');
+  const [question1, setQuestion1] = useState("");
+  const [question2, setQuestion2] = useState("");
+  const [question3, setQuestion3] = useState("");
+  const [question4, setQuestion4] = useState("");
+
+  const [value, setAnswer] = useState("");
+  const [question, setQuestion] = useState("");
 
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
-  const cityName = searchParams.get('cityName');
+  const cityName = searchParams.get("cityName");
   console.log(cityName);
 
+  const token = localStorage.getItem("token");
   const handleClick = (event) => {
-    const token = localStorage.getItem("token");
     event.preventDefault();
     const answer = event.target.value;
     setAnswer(answer);
     console.log(answer);
-
   };
 
   useEffect(() => {
-    fetch("/api/question/", {
-      method: "POST",
+    fetch(`/api/user/cities/question?cityName=${cityName}`, {
+      method: "GET",
       headers: {
         "Content-Type": "application/json",
         Authorization: `${token}`,
       },
-      body: JSON.stringify({ answer, cityName }),
     }).then((res) => {
-      if (res.status !== 201) {
+      if (res.status !== 200) {
       } else {
         res.json().then((data) => {
-          console.log(data);
+          const answers = shuffleArray(
+            data.question.offered_answers.split(";")
+          );
+          console.log(answers);
           setQuestion(data.question.text);
+          setQuestion1(answers[0]);
+          setQuestion2(answers[1]);
+          setQuestion3(answers[2]);
+          setQuestion4(answers[3]);
         });
       }
     });
-  })
+  });
 
   const questionForm = (
     <div className="container">
@@ -47,20 +65,42 @@ function Question() {
       <div className="question-text">{question}</div>
       <div className="inputs">
         <div className="button-container">
-          <button className="answer-button" name="answer-button" onClick={handleClick} value={"question 1"}>Question 1</button>
+          <button
+            className="answer-button"
+            name="answer-button"
+            onClick={handleClick}
+            value={"question 1"}>
+            {question1}
+          </button>
         </div>
         <div className="button-container">
-          <button className="answer-button" name="answer-button" onClick={handleClick} value={"question 2"}>Question 2</button>
+          <button
+            className="answer-button"
+            name="answer-button"
+            onClick={handleClick}
+            value={"question 2"}>
+            {question2}
+          </button>
         </div>
         <div className="button-container">
-          <button className="answer-button" name="answer-button" onClick={handleClick} value={"question 3"}>Question 3</button>
+          <button
+            className="answer-button"
+            name="answer-button"
+            onClick={handleClick}
+            value={"question 3"}>
+            {question3}
+          </button>
         </div>
         <div className="button-container">
-          <button className="answer-button" name="answer-button" onClick={handleClick} value={"question 4"}>Question 4</button>
+          <button
+            className="answer-button"
+            name="answer-button"
+            onClick={handleClick}
+            value={"question 4"}>
+            {question4}
+          </button>
         </div>
       </div>
-
-
     </div>
   );
 
